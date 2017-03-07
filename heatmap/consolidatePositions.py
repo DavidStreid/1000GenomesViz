fa16112e7bbdaf48c37307620560625ae827b188
David Streid <dcs2153@columbia.edu>
2017-03-07 06:48:45 -0800

    script to count frequency of mutations

---
+++ heatmap/consolidatePositions.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import re
+import json
+
+# Converts file in the following format to JSON -
+#     INPUT
+#         Population,Sample,Mut,Severity,col,row
+#         ACB,HG02052,chrMT-990-C,2.0,110,23
+#         ACB,HG02325,chrMT-1007-A,1.0,2,45
+#     OUTPUT
+#         {"count": 32, "pos": "15784", "change": "C", "severity": "-1.0"}
+#         {"count": 4, "pos": "15431", "change": "A", "severity": "-1.0"}
+#         {"count": 1, "pos": "15470", "change": "C", "severity": "-1.0"}
+# $ python consolidatePositions.py . consolidatedPositions/
+
+if(len(sys.argv) < 3):
+    print "ERROR: Specify an input and output directory"
+    sys.exit()
+
+heatMapDirectory = sys.argv[1]
+targetDirectory = sys.argv[2]
+
+for filename in os.listdir(heatMapDirectory):
+    searchObj = re.search( r'_heatmap.csv', filename, re.M|re.I) #Only heatmap data
+    if(searchObj):
+        with open(filename,'r') as i:
+            lines = i.readlines()
+            data = lines[1:]
+            count = 1
+            if(data):   # If file contains non-header information
+                posDic = {}
+
+                pop = data[0].split(',')[0] # Population should be same for each file
+
+                for d in data:              # Iterate through non-header information
+                    mutData = d.split(',')  
+                    posData = mutData[2].split('-') # split on dashes (E.g. chrMT-1007-A)
+                    loc = posData[1]                # basepair position
+                    mut = posData[2]                # base change
+                    if(posDic.has_key(loc)):        # Get frequency at position
+                        posDic[loc][0] += 1
+                    else:
+                        posDic[loc] = [1, mutData[3], mut]  #[count, severity, base change]
+
+                # Write to file
+                file = targetDirectory + '/' + pop + '_mutFreq.json'
+                target = open(file, 'w')
+                for k, v in posDic.iteritems():     
+                    target.write(json.dumps({'pos': k, 'count': v[0], 'severity': v[1], 'change': v[2]}) + '\n')
